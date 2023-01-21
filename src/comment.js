ac6004a1ce52f0bc9436803e519c29faf956b777
RuralAnemone <dev@ruralanemone.tech>
2023-01-21 14:45:27 -0600

    GAH DANG I THOUGHT IT COMMITTED

---
+++ src/comment.js
@@ -0,0 +1,84 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+
+/*  didn't really feel like adding css but oh well
+*   I think it got away from me a bit :)
+*   still works though
+*   lol
+*/
+
+function Avatar(props) {
+    return (
+        <div style={{
+            "display": "flex",
+            "flex-direction": "row",
+            "align-items": "baseline",
+        }}>
+            <img
+                src={props.user.avatarUrl}
+                alt={props.user.name}
+                style={{
+                    "border-radius": "100%",
+                    "width": 50,
+                    "height": 50,
+                }}
+            />
+            <p>{props.user.name}</p>
+        </div>
+    );
+}
+
+function Text(props) {
+    return (
+        <pre>{props.text}</pre>
+    );
+}
+
+function FormattedDate(props) {
+    return (
+        <p>{new Date(props.time).toDateString()}</p>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div>
+            <Avatar user={props.user} />
+            <FormattedDate time={props.user.time /* oh who cares it's just an exmaple lol */} />
+            <Text text={props.user.text} />
+            <hr/>
+        </div>
+    );
+}
+
+// ----
+// users
+
+const greg123 = {
+    avatarUrl: "https://placebear.com/50/50",
+    name: "greg123",
+    time: Date.now(),
+    text: "comment? lorem ipsum dolor sit amet. sussy amon gus!",
+}
+
+const sharon456 = {
+    avatarUrl: "https://placekitten.com/50/50",
+    name: "sharon456",
+    time: new Date(0),
+    text: "I tihnk I accidentally used Greg and Sharon from Futilitycloset... uh... :)",
+}
+
+// -----
+
+function App() {
+    return (
+        <div>
+            <Comment user={greg123} />
+            <Comment user={sharon456} />
+        </div>
+    );
+}
+
+root.render(<App />);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() };
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <code>it is currently {this.state.date.toLocaleString()}. guh!</code>
+        );
+    }
+}
+
+root.render(<Clock />);
